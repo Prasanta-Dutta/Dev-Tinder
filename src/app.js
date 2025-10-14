6f4a32f5d5b0d4409f659c2c59f3853f68f43891
Prasanta Dutta <prasanta0dutta@gmail.com>
2025-10-14 21:55:58 +0530

    1. Login feature built 2. Encrypt password and compare using bcrypt

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const { PORT } = require("./constants");
 const { connectDB } = require("./utils");
 const { Users } = require("./models");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -48,13 +49,56 @@ app.post("/user/api/V0/signup", async (req, res, next) => {
             return res.send("Mobile is not valid");
         }
 
-        const user = new Users({firstName, lastName, email, mobile, gender, age, password});
+        const isUserExist = await Users.findOne({email});
+        
+        if(isUserExist){
+            return res.send("User already exist"); // Invalid cradentials
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 9);
+
+        const user = new Users({firstName, lastName, email, mobile, gender, age, password: hashedPassword});
         const result = await user.save();
         console.log(result);
-        return res.send("API call successfull");
+        if(result){
+            return res.send("User successfully registered");
+        }
+        return res.send("Failed to register user");
     }
     catch(err){
         return res.send("Error: " + err.message);
+    }
+});
+
+app.post("/user/api/V0/signin", async (req, res) => {
+    for(const key in req.body){
+        if(typeof(req.body[key]) === "string"){
+            req.body[key] = req.body[key].trim();
+        }
+    }
+
+    const {email, password} = req.body;
+
+    if([email, password].some((value) => {
+        return value === undefined || value === "" || value === null;
+    })){
+        return res.send("Email or password can not be empty");
+    }
+
+    if(!validator.isEmail(email)){
+        return res.send("Invalid email");
+    }
+
+    const isUserExisted = await Users.findOne({email});
+    if(!isUserExisted){
+        return res.send("Invalid credential");
+    }
+
+    if(await bcrypt.compare(password, isUserExisted.password)){
+        return res.send("Login successful");
+    }
+    else{
+        return res.send("Invalid credential");
     }
 });
 
